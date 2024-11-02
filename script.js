1cefe3b1c2addd5aad2a6b65b47698aa69c816a8
Suvam6262 <suvendumahapatra69@gmail.com>
2024-11-02 23:37:51 +0530

    Added basic JavaScript logics

--- script.js
+++ script.js
@@ -2,18 +2,41 @@ let rockBtn = document.getElementById("btn-Rock");
 let paperBtn = document.getElementById("btn-Paper");
 let scissorBtn = document.getElementById("btn-Scissor");
 
-let p1Score = document.getElementById("p1-score");
-let p2Score = document.getElementById("p2-score");
+let playersScore = document.getElementById("p1-score");
+let compsScore = document.getElementById("p2-score");
 
 
-let count  = 0;                //Click Count
-const scoreCount = () =>{
-    count++;
-};
+let playersClickCount = 0;
+let compsClickCount = 0;
+// Winning Logic
+let playRound = (playerChoice) => {
+    let compsChoice = compsTurn();
+    console.log(`Player chose: ${playerChoice} and Computer chose: ${compsChoice}`);
+
+    if (playerChoice === compsChoice) {
+        console.log("It's a tie.");
+    } else if (playerChoice === "Rock" && compsChoice === "Scissor" ||
+        playerChoice === "Paper" && compsChoice === "Rock" ||
+        playerChoice === "Scissor" && compsChoice === "Paper") {
+            playersClickCount ++;
+            playersScore.innerText = playersClickCount;
+            console.log("Congrats! You are the Winner");
+    }else{
+        compsClickCount ++;
+            compsScore.innerText = compsClickCount;
+            console.log("Computer wins the match");
+    }
+}
 
 
-const compTurn = () =>{                                  //Computer's Turn
+const compsTurn = () => {                                  //Computer's Turn
     let compOptions = ["Rock", "Paper", "Scissor"];
     option = Math.floor(Math.random() * compOptions.length);
     return compOptions[option];
 }
+
+//Added Evnet listenr for Player's Turn
+rockBtn.addEventListener("click", () => { playRound("Rock") })
+paperBtn.addEventListener("click", () => { playRound("Paper") })
+scissorBtn.addEventListener("click", () => { playRound("Scissor") })
+
